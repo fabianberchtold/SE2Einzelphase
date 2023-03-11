e2c303e375d8bcc7206279a3ba261e8480d6327d
fabianberchtold <127214759+fabianberchtold@users.noreply.github.com>
2023-03-11 12:29:32 +0100

    Changed input type of login to numeric and linked some views with code

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private Button abschicken;
+    private EditText matrikelnummer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        abschicken.findViewById(R.id.button);
+        matrikelnummer.findViewById(R.id.login);
     }
 }
